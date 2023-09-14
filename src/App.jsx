c50739023b5b6bca5a8e3265f8d2703496ba585a
Hari <kathulaharish929311@gmail.com>
2023-09-14 19:28:32 +0530

    index.css file updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import Experience from "./components/experience/Experience";
 import Services from "./components/services/Services"
 import Portfolio from "./components/portfolio/Portfolio"
 import Testimonials from "./components/testimonials/Testimonials"
+import Contact from "./components/contacts/Contact"
+import Footer from "./components/footer/Footer"
+
 
 const App = ()=>{
     return (
@@ -17,6 +20,9 @@ const App = ()=>{
             <Services/>
             <Portfolio/>
             <Testimonials/>
+            <Contact/>
+            <Footer/>
+
         </>
     )
 }
