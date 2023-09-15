c351c8bd1655e4883b56f7225aa9436bc34f95a7
Hari <kathulaharish929311@gmail.com>
2023-09-15 16:46:15 +0530

    footer added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 import "./footer.css"
+import {AiOutlineFacebook} from "react-icons/ai"
+import {BsInstagram} from "react-icons/bs"
+import {FaTwitter} from "react-icons/fa"
 
 const Footer = ()=>{
     return(
-        <div>Footer</div>
+        <footer>
+            <a href="#" className="footer_logo">Hari</a>
+
+            <ul className="permalinks">
+                <li><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#testimonials">Testimonials</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+
+            <div className="footer_socials">
+                <a href="https://facebook.com"><AiOutlineFacebook/></a>
+                <a href="https://instagram.com"><BsInstagram/></a>
+                <a href="https://twitter.com"><FaTwitter/></a>
+            </div>
+
+            <div className="footer_copyright">
+                <small>&copy; All rights reserved.</small>
+            </div>
+        </footer>
     )
 }
 
